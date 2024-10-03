77d433aad4da5a5b1923d8f105920bd0e3388594
Leonardo Favio Jimenez Layme <leonardofavio454@gmail.com>
2024-10-03 12:27:03 -0500

    Cambios importantes * Sera una pagina de motos * Tendra un catalogo

--- src/componentes/header/Header.jsx
+++ src/componentes/header/Header.jsx
@@ -3,8 +3,16 @@ import '../../estilos/header.css'
 function Header() {
     return (
         <div className='contendor-header'>
-            <div className="logo-contendor">
-                <p className='mover'>Empresa <span>Amarilla</span></p>
+            <div className='items-contendor'>
+                <div className="logo-contendor">
+                    <p>Empresa <br/>Amarilla</p>
+                </div>
+                <div className='nav'>
+                    <a href="">catalogo de motos</a>
+                    <a href="">accesorios</a>
+                    <a href="">financiamiento</a>
+                    <a href="">ventas corporativas</a>
+                </div>
             </div>
             <input type="button" value="REGISTRATE" />
         </div>
